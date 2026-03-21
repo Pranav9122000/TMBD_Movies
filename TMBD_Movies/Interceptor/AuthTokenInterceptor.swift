7b1de0eb892f937a30effb095a9aef61c1996377
Kulkarni, Pranav <pranavthor2000@gmail.com>
2026-03-21 16:14:08 +0530

    Update AuthTokenInterceptor.swift

--- TMBD_Movies/Interceptor/AuthTokenInterceptor.swift
+++ TMBD_Movies/Interceptor/AuthTokenInterceptor.swift
@@ -13,7 +13,7 @@ class AuthTokenInterceptor: RequestInterceptor {
     func intercept(_ request: URLRequest) async throws -> URLRequest {
         var modifiedRequest = request
         
-        let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOWRiZjc2YjQ3MGRkYjEwYTExMTA0ODAwMzdhN2U2ZCIsIm5iZiI6MTcwODE4ODQwNy42NzM5OTk4LCJzdWIiOiI2NWQwZTJmNzY5YzcwZjAxNGE5ODFkZDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.HXm1Xb3Cm3rZxfywYQyJj1NSzGhG5uRl-2szfjoQiZg"
+        let token = ""
         
         modifiedRequest.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         
